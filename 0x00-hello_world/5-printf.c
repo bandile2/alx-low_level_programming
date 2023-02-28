37a4a11dc140df3a12742e99b6264cad3db56dc1
bandile2 <bandilemlangeni@gmail.com>
2023-02-28 14:55:35 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 /**
  * main - prints "with proper grammar, but the outcome is
  * a piece of art," followed by a new line.
- * Return: Always 0.
- * 
+ * Return: Always 0. 
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
