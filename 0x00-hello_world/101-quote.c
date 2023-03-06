aa91dcc7f70117e0e1763b0e0ed03b693dbca8fd
bandile2 <bandilemlangeni@gmail.com>
2023-03-06 03:48:53 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program thst prints a linr to the standard e
+rror
+* Return 1 (Success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Kor
+par, 2015.10.19\n", stdout);
+return (1);
